2a608d122afc704023cda80c2da81cefa8949f80
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2022-06-24 05:40:33 +0700

    Categories API

---
+++ config/app.config.js
@@ -0,0 +1,8 @@
+const MONGO_DB_CONFIG = {
+	DB: 'mongodb+srv://radhasjt:Cadenza97256@cluster0.mqrtu.mongodb.net/grocery_db?retryWrites=true&w=majority',
+	PAGE_SIZE: 10,
+};
+
+module.exports = {
+	MONGO_DB_CONFIG,
+}
