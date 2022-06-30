839aae2844df53f4065756d77f9f12ffcf15fa44
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2022-06-30 21:25:31 +0700

    Update api login register

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,5 +1,7 @@
 const categoryController = require('../controllers/categories.controller');
 const productController = require('../controllers/products.controller');
+const userController = require('../controllers/users.controller');
+
 const express = require('express');
 const router = express.Router();
 
@@ -14,5 +16,8 @@ router.get('/product', productController.findAll);
 router.get('/product/:id', productController.findOne);
 router.put('/product/:id', productController.update);
 router.delete('/product/:id', productController.delete);
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 module.exports = router;
